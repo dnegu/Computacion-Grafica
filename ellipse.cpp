c6e4a94ec42184a1227c26fa37abd879cb207207
David Neyra Gutierrez <davidombu@gmail.com>
2016-10-07 13:19:40 -0500

    Update ellipse.cpp

--- ellipse.cpp
+++ ellipse.cpp
@@ -1,9 +1,7 @@
+
 #include<stdio.h>
-
 #include<GL/gl.h>
-
 #include<GL/glu.h>
-
 #include<GL/glut.h>
 
 void ellipse()
@@ -69,37 +67,23 @@ void ellipse()
         glFlush();
              
         }
-        }
+}
  void Init(){
-
-        glViewport(0 , 0 , 640 , 480);
-
+          glViewport(0 , 0 , 640 , 480);
           glMatrixMode(GL_PROJECTION);
-
           glLoadIdentity();
-
           gluOrtho2D(0 , 640 , 0 , 480);
 }
 
 int main(int argc, char **argv)
-
 {
-
          glutInit(&argc,argv);
-
          glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-
          glutInitWindowSize(640,480);
-
          glutInitWindowPosition(0,0);
-
          glutCreateWindow("Ellipse");
-
          Init();
-
          glutDisplayFunc(ellipse);
-
          glutMainLoop();
-
          return 0;
 }  
